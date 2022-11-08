ed512fbf1903771ead717165ae64e3f73b627c8f
jackjchou <jackjchou@tencent.com>
2022-11-08 20:47:20 +0800

    php:array内置函数和oop

---
+++ base/builtin_func/array_chunk.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * array_chunk() 函数把一个数组分割为新的数组块。
+ *
+ * 语法
+ * array_chunk(array,size,preserve_keys);
+ *
+ * 参数    描述
+ * array    必需。规定要使用的数组。
+ * size    必需。一个整数，规定每个新数组块包含多少个元素。
+ * preserve_key    可选。可能的值：
+ * true - 保留原始数组中的键名。
+ * false - 默认。每个新数组块使用从零开始的索引。
+ */
+
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43", "Harry" => "50");
+print_r(array_chunk($age, 2, true));
+print_r(array_chunk($age, 2));
+
+$arr = array_chunk($age, 2, true);
+foreach ($arr as $arrTemp) {
+    var_dump($arrTemp);
+}
---
+++ base/oop/dbService.php
@@ -0,0 +1,35 @@
+<?php
+
+class DbService
+{
+    public function getSalesGroupListByCondition(
+        $fields = array(),
+        $where = [],
+        $like = [],
+        $limit = null,
+        $offset = null,
+        $sort = null,
+        $sort_type = 'DESC',
+        $needCount = true
+    )
+    {
+        print("接口被调用\n");
+        print_r($fields);
+        print_r($where);
+
+    }
+
+    public function todo() {
+        $rule = array("groupIds" => "1,2,3,4");
+        //在类中调用自身方法
+        self::getSalesGroupListByCondition([], ['groupId' => explode(',', $rule['groupIds'])]);
+    }
+
+}
+
+$my = new DbService();
+$my->todo();
+
+//这里为了测试一下，在函数定义的时候如果都给了默认值，在调用的时候，可以不传参
+//$my->getSalesGroupListByCondition([], ['groupId' => explode(',', $rule['groupIds'])]);
+
